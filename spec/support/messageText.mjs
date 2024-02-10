65f7b745bd26abf0fc790fcae8a6bbc05d31eb54
Howard (Aech Linden) Stearns <howard.stearns@gmail.com>
2024-02-10 10:51:02 -0800

    smaller test message crept in

--- spec/support/messageText.mjs
+++ spec/support/messageText.mjs
@@ -1,4 +1,4 @@
-export const scale = /*10 **/ 1024 * 1024;
+export const scale = 10 * 1024 * 1024;
 export function makeMessage(length = scale) {
   return Array.from({length}, (_, index) => index & 1).join('');
 }

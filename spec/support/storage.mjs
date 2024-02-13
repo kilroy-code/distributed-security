1f0fb84d3dbc4a39ad1af4e2e8fa7509b0de9e79
Howard (Aech Linden) Stearns <howard.stearns@gmail.com>
2024-02-13 07:39:21 -0800

    bottleneck storage

--- spec/support/storage.mjs
+++ spec/support/storage.mjs
@@ -1,8 +1,9 @@
 import Security from "../../lib/security.mjs";
 const Storage = {
-  async store(resourceTag, ownerTag, string, signature) {
-    // Note: This trivial storage mechanism assumes one ownerTag per resourceTag, and the resourceTag is a global string. A real resourceTag is likely to be a path.
-    if (!await Security.verify(signature, ownerTag)) throw new Error(`Signature ${signature} for ${string} does not match owner of ${ownerTag}.`);
+  async store(resourceTag, ownerTag, _string, signature) {
+    let verified = await Security.verify(signature, ownerTag),
+	string = verified?.text;
+    if (!verified) throw new Error(`Signature ${signature} for ${string} does not match owner of ${ownerTag}.`);
     if (!string) {
       // FIXME? Is it ever meaningful to store an empty payload? If so, we'll need a separate delete operation.
       delete this[resourceTag][ownerTag];

fe949ad47e7e9485caa8aa24d9b724eec79ecb2f
Howard (Aech Linden) Stearns <howard.stearns@gmail.com>
2024-02-14 08:53:59 -0800

    checkpoint

--- spec/support/storage.mjs
+++ spec/support/storage.mjs
@@ -1,10 +1,9 @@
-import Security from "../../lib/security.mjs";
+import Security from "../../index.mjs";
 const Storage = {
   async store(resourceTag, ownerTag, signature) {
-    // let verified = await Security.verify(signature, ownerTag),
-    // 	string = verified?.text;
-    // if (!verified) throw new Error(`Signature ${signature} for ${string} does not match owner of ${ownerTag}.`);
-    if (!signature.split('.')[1]) {
+    let verified = await Security.verify(signature, ownerTag);
+    if (!verified) throw new Error(`Signature ${signature} does not match owner of ${ownerTag}.`);
+    if (!verified.text) {
       // FIXME? Is it ever meaningful to store an empty payload? If so, we'll need a separate delete operation.
       delete this[resourceTag][ownerTag];
       return null;

c63b1fa4d7776a9a771eb63404305940fb138cab
Howard (Aech Linden) Stearns <howard.stearns@gmail.com>
2024-02-27 07:53:42 -0800

    restore validation in storage

--- spec/support/storage.mjs
+++ spec/support/storage.mjs
@@ -1,20 +1,13 @@
-import Security from "../../index.mjs";
 const Storage = {
   async store(resourceTag, ownerTag, signature) {
-    let verified = await Security.verify(signature, {team: ownerTag, notBefore: 'team'});
+    let verified = await this.Security.verify(signature, {team: ownerTag, notBefore: 'team'});
     if (!verified) throw new Error(`Signature ${signature} does not match owner of ${ownerTag}.`);
-    if (!verified.payload.length) {
+    if (verified.payload.length) {
+      this[resourceTag][ownerTag] = signature;
+    } else {
       delete this[resourceTag][ownerTag];
-      return null;
     }
-    this[resourceTag][ownerTag] = signature;
-    // We were given the signature, and not the payload. By returning the original payload, we
-    // give the caller a reasonable expectation that the storage implementation -- which
-    // the app does not necessarily control -- has examined the signature in some way.
-    // Note that json will be define if and only if the signature cty specifies json,
-    // and otherwise text will be defined if cty specifies text.
-    // Distributed-Storage does not depend on this returned value, but it is nice practice.
-    return verified.json ?? verified.text ?? verfied.payload;
+    return null; // Must not return undefined for jsonrpc.
   },
   async retrieve(resourceTag, ownerTag) {
     // We do not verify and get the original data out here, because the caller has

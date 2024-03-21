b0cc5826591985dadcfcbda39027929fdd71a99a
Howard (Aech Linden) Stearns <howard.stearns@gmail.com>
2024-03-20 19:45:45 -0700

    same

--- spec/support/messageText.mjs
+++ spec/support/messageText.mjs
@@ -6,3 +6,9 @@ const base64withDot = /^[A-Za-z0-9_\-\.]+$/;
 export function isBase64URL(string, regex = base64withDot) {
   expect(regex.test(string)).toBeTruthy();
 }
+
+export function sameTypedArray(result, message) {
+  // The payload is a Uint8Array, but in NodeJS, it will be a subclass of Uint8Array,
+  // which won't compare the same in Jasmine toEqual.
+  expect(new Uint8Array(result.payload)).toEqual(message);
+}

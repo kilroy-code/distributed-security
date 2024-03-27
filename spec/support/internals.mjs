7630d3eca72281b10da387b68276417ce5762f4b
Howard (Aech Linden) Stearns <howard.stearns@gmail.com>
2024-03-26 21:21:58 -0700

    forgot one

--- spec/support/internals.mjs
+++ spec/support/internals.mjs
@@ -2,6 +2,6 @@ import Krypto from "../../lib/krypto.mjs";
 import MultiKrypto from "../../lib/multiKrypto.mjs";
 import InternalSecurity from "../../lib/api.mjs";
 import {KeySet, DeviceKeySet, TeamKeySet} from "../../lib/keySet.mjs";
-import dispatch from "../../dependency/jsonrpc.mjs";
+import dispatch from "@kilroy-code/jsonrpc";
 import LocalCollection from "#localStore";
 export {Krypto, MultiKrypto, InternalSecurity, KeySet, DeviceKeySet, TeamKeySet, dispatch, LocalCollection};
